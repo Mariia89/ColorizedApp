920da981cb530adc3c5049413ac41a8190b9c150
m.chudakova <masterova89@mail.ru>
2022-06-26 17:51:20 +0300

    1

--- ColorizedApp/ViewController.swift
+++ ColorizedApp/ViewController.swift
@@ -8,12 +8,20 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var mainView: UIView!
+    
+    @IBOutlet weak var redSlider: UISlider!
+    
+    @IBOutlet weak var greenSlider: UISlider!
+    @IBOutlet weak var blueSlider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    private func setupMainView() {
+        mainView.layer.cornerRadius = 10
+    }
 
 }
 
