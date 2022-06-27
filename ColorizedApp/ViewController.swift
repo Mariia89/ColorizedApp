d7983ffad1f7680a989ec3724f75050ba642b05b
m.chudakova <masterova89@mail.ru>
2022-06-27 23:32:31 +0300

    1

--- ColorizedApp/ViewController.swift
+++ ColorizedApp/ViewController.swift
@@ -8,20 +8,42 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
     @IBOutlet weak var mainView: UIView!
     
-    @IBOutlet weak var redSlider: UISlider!
+    @IBOutlet weak var redLabel: UILabel!
+    @IBOutlet weak var greenLabel: UILabel!
+    @IBOutlet weak var blueLabel: UILabel!
     
+    @IBOutlet weak var redSlider: UISlider!
     @IBOutlet weak var greenSlider: UISlider!
     @IBOutlet weak var blueSlider: UISlider!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
-    private func setupMainView() {
+        
         mainView.layer.cornerRadius = 10
+        mainView.backgroundColor = UIColor(
+            red: CGFloat(redSlider.value),
+            green: CGFloat(greenSlider.value),
+            blue: CGFloat(blueSlider.value),
+            alpha: 1
+        )
+        redLabel.text = String(format: "%.f2", redSlider.value)
+        greenLabel.text = String(format: "%.f2", greenSlider.value)
+        blueLabel.text = String(format: "%.f2", blueSlider.value)
     }
-
+    
+    @IBAction func sliderAction(_ sender: UISlider) {
+        mainView.backgroundColor = UIColor(
+            red: CGFloat(redSlider.value),
+            green: CGFloat(greenSlider.value),
+            blue: CGFloat(blueSlider.value),
+            alpha: 1
+        )
+        redLabel.text = String(format: "%.f2", redSlider.value)
+        greenLabel.text = String(format: "%.f2", greenSlider.value)
+        blueLabel.text = String(format: "%.f2", blueSlider.value)
+    }
 }
-
